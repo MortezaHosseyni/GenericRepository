db425376936a2d9c7e7ceb127981093613413877
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2023-11-03 14:20:44 +0330

    Generic Repository Class Create generic repository class methods - FindById - GetAll - GetAllWithInclude - GetLimitedData - Find - FindInfinity - FindOne - Count - CountByPredicate - Add - AddRange - Update - UpdateRange - Remove - RemoveRange

--- IGenericRepository.cs
+++ IGenericRepository.cs
@@ -20,4 +20,111 @@ public interface IGenericRepository<TEntity> where TEntity : class
 
 public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
 {
+    protected readonly AppDbContext _context;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task<TEntity> GetByGuidAsync(string guid)
+    {
+        return await _context.Set<TEntity>().FindAsync(Guid.Parse(guid));
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _context.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllWithIncludeAsync(params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = _context.Set<TEntity>();
+
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetLimitedDataAsync(int limit, string orderByPropertyName)
+    {
+        var entityType = typeof(TEntity);
+        var parameter = Expression.Parameter(entityType, "x");
+        var property = Expression.Property(parameter, orderByPropertyName);
+        var lambda = Expression.Lambda<Func<TEntity, object>>(Expression.Convert(property, typeof(object)), parameter);
+
+        return await _context.Set<TEntity>()
+            .OrderBy(lambda)
+            .Take(limit)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().AsNoTracking().Where(predicate).ToListAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> FindInfinityAsync(Expression<Func<TEntity, bool>> predicate, int skip, int take)
+    {
+        return await _context.Set<TEntity>().Skip(skip).Take(take).AsNoTracking().Where(predicate).ToListAsync();
+    }
+
+    public async Task<TEntity> FindOneAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().AsNoTracking().Where(predicate).FirstOrDefaultAsync();
+    }
+
+    public async Task<long> CountAsync()
+    {
+        return await _context.Set<TEntity>().AsNoTracking().CountAsync();
+    }
+
+    public async Task<long> CountByPredicateAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().AsNoTracking().CountAsync(predicate);
+    }
+
+    public void Add(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().AddRange(entities);
+        _context.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+        _context.SaveChanges();
+    }
+
+    public void UpdateRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().UpdateRange(entities);
+        _context.SaveChanges();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+        _context.SaveChanges();
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().RemoveRange(entities);
+        _context.SaveChanges();
+    }
 }
